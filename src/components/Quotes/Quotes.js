f7fede96d76d09f1ce13f0edcba0d8cac80da42c
chaitanya-001 <chaitanya.munipalli@badbeat.com>
2024-09-01 19:57:54 +0530

    books page added

--- src/components/Quotes/Quotes.js
+++ src/components/Quotes/Quotes.js
@@ -1,17 +1,49 @@
 import React from "react";
 import "./Quotes.css";
+import { Carousel } from "react-responsive-carousel";
 
 const Quotes = () => {
-  return (
-    <div className="quotes-container">
-      <div className="quotes-content">
-        <blockquote>
-          Read as if you're finding the writer's heart <br />
-          Write like you're reading the reader's mind
-        </blockquote>
-        <p className="author">-- Dr. Sloka</p>
-      </div>
-    </div>
+  return (<div className="quotes-container-wrapper">
+    <h3 className="mb-4"> TRENDING QUOTES</h3>
+    <Carousel
+      showArrows={false}
+      showStatus={false}
+      autoPlay
+      infiniteLoop
+      centerMode={true}
+      showThumbs={false} >
+      <div className="quotes-container">
+        <div className="quotes-content">
+          <blockquote>
+            <p className="quotes-text">
+              <p className="quotes-text">
+                Read as if you're finding the writer's heart
+              </p>
+              <p className="quotes-text">
+                Write like you're reading the reader's mind
+              </p>
+            </p>
+          </blockquote>
+          <p className="author">-- Dr. Sloka</p>
+        </div>
+        </div>
+        <div className="quotes-container">
+          <div className="quotes-content">
+            <blockquote>
+              <p className="quotes-text">
+                <p className="quotes-text">
+                  Progression in Life need Initiation
+                </p>
+                <p className="quotes-text">
+                  The seed of Initiation is Determination
+                </p>
+              </p>
+            </blockquote>
+            <p className="author">-- Dr. Sloka</p>
+          </div>
+        </div>
+    </Carousel>
+  </div>
   );
 };
 
--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -4,12 +4,25 @@ import GreetingBanner from "../../components/GreetingBanner/GreetingBanner";
 import WelcomeBanner from "../../components/WelcomeBanner.js/WelcomeBanner";
 import Quotes from "../../components/Quotes/Quotes";
 import ContactForm from "../ContactForm/ContactForm";
+import ScrollComponent from "../../components/ScrollComponent/ScrollComponent";
+import sloka from '../sloka.json'
+
+const homepage = sloka.homepage
 
 const HomePage = () => {
+  const renderScrollComponent = () => homepage.map((obj, index) => {
+    if (obj.renderType === "scroll_images") {
+      return <ScrollComponent {...obj} key={`scroll_images_${index}`} />
+    }
+    return <></>
+  })
   return (
     <div className="homepage-container">
       <GreetingBanner />
       <WelcomeBanner />
+      <div id='sroll_image_wrapper'>
+        {renderScrollComponent()}
+      </div>
       <Quotes />
       <ContactForm />
     </div>
