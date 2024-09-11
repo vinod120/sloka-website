6bd971fc4af91a3cad66f9fd2bcc8cfa2c2052ec
arkavinod <vinod.kumar@badbeat.com>
2024-09-11 22:39:18 +0530

    issue fixes

--- src/pages/Books/BooksCarousel.js
+++ src/pages/Books/BooksCarousel.js
@@ -15,20 +15,23 @@ const BooksCarousel = () => {
           <h4 className="text-primary">Dr. Sloka's</h4>
           <h1 className="display-5 text-white mb-4">Books</h1>
           <p className="text-white mb-0">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-            adipisci facilis cupiditate recusandae aperiam temporibus corporis
-            itaque quis facere, numquam, ad culpa deserunt sint dolorem autem
-            obcaecati, ipsam mollitia hic.
+            Transform your thoughts and life, nourish your mind and soul.
+            <br />
+            Explore Dr. Sloka’s books and start your journey to empower yourself
+!
           </p>
         </div>
 
         {/* Swiper Carousel */}
         <Swiper
           modules={[Navigation, Autoplay]}
-          autoplay={{ delay: 2000 }}
+          // autoplay={{ delay: 2000 }}
           speed={2000}
           loop={true}
-          navigation={{ nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' }}
+          navigation={{
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev"
+          }}
           spaceBetween={25}
           breakpoints={{
             0: { slidesPerView: 1 },
@@ -36,15 +39,15 @@ const BooksCarousel = () => {
             768: { slidesPerView: 2 },
             992: { slidesPerView: 2 },
             1200: { slidesPerView: 2 },
-            1400: { slidesPerView: 2 },
+            1400: { slidesPerView: 2 }
           }}
         >
           {/* Carousel Items */}
-          <SwiperSlide>
+          <SwiperSlide className="textstt">
             <div className="attractions-item">
               <img
                 src="https://drslokauk.netlify.app/images/the_unspoken_thoughts.jpg"
-                className="img-fluid books-img rounded w-100"
+                className="books-img rounded"
                 alt="The Unspoken Thoughts"
               />
               <a href="#" className="attractions-name">
@@ -53,11 +56,11 @@ const BooksCarousel = () => {
             </div>
           </SwiperSlide>
 
-          <SwiperSlide>
+          <SwiperSlide className="textstt">
             <div className="attractions-item">
               <img
                 src="https://drslokauk.netlify.app/images/not_just_sadness.jpg"
-                className="img-fluid books-img rounded w-100"
+                className="books-img rounded"
                 alt="Not Just Sadness"
               />
               <a href="#" className="attractions-name">
@@ -66,11 +69,11 @@ const BooksCarousel = () => {
             </div>
           </SwiperSlide>
 
-          <SwiperSlide>
+          <SwiperSlide className="textstt">
             <div className="attractions-item">
               <img
-                src="https://drslokauk.netlify.app/images/the_unspoken_thoughts.jpg"
-                className="img-fluid books-img rounded w-100"
+                src="https://placehold.co/1800x1200"
+                className="books-img rounded"
                 alt="The Unspoken Thoughts"
               />
               <a href="#" className="attractions-name">
@@ -79,11 +82,11 @@ const BooksCarousel = () => {
             </div>
           </SwiperSlide>
 
-          <SwiperSlide>
+          <SwiperSlide className="textstt">
             <div className="attractions-item">
               <img
                 src="https://drslokauk.netlify.app/images/not_just_sadness.jpg"
-                className="img-fluid books-img rounded w-100"
+                className="books-img rounded"
                 alt="Not Just Sadness"
               />
               <a href="#" className="attractions-name">
